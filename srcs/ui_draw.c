7dd1e5d45f34ec7c963dea0f56a39e4f1a03dae3
Mattia Torsello <mtorsell@student.42.fr>
2019-03-16 17:24:20 +0100

    renamed file

---
+++ srcs/ui_draw.c
@@ -0,0 +1,52 @@
+#include "doom.h"
+
+void    draw_weapon(SDL_Surface *surface, SDL_Surface *weapon)
+{
+    int x;
+    int y;
+
+    y = 0;
+    while (y < weapon->h)
+    {
+        x = 0;
+        while (x < weapon->w)
+        {
+            put_pixel(surface, HALF_W - (weapon->w / 2) + x, WIN_H - weapon->h + y, int_to_t_color(get_pixel(weapon, x, y, t_false)));
+            x++;
+        }
+        y++;
+    }
+}
+
+void    draw_crosshair(SDL_Surface *surface, Uint32 color)
+{
+    t_segment   segment;
+
+    segment.x1 = HALF_W;
+    segment.y1 = HALF_H - 30;
+    segment.x2 = HALF_W;
+    segment.y2 = HALF_H - 30 - 50;
+    draw_segment(surface, segment, color);
+    segment.x1 = HALF_W - 30;
+    segment.y1 = HALF_H;
+    segment.x2 = HALF_W - 30 - 50;
+    segment.y2 = HALF_H;
+    draw_segment(surface, segment, color);
+    segment.x1 = HALF_W;
+    segment.y1 = HALF_H + 30;
+    segment.x2 = HALF_W;
+    segment.y2 = HALF_H + 30 + 50;
+    draw_segment(surface, segment, color);
+    segment.x1 = HALF_W + 30;
+    segment.y1 = HALF_H;
+    segment.x2 = HALF_W + 30 + 50;
+    segment.y2 = HALF_H;
+    draw_segment(surface, segment, color);
+    put_pixel(surface, HALF_W, HALF_H, int_to_t_color(color));
+}
+
+void    ui_draw(t_sdl *sdl, SDL_Surface *weapon)
+{
+    draw_crosshair(sdl->surface, CROSSHAIR_COLOR);
+    draw_weapon(sdl->surface, weapon);
+}
