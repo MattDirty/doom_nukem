867ba51a08225c0a6c20db751fc78fe10a1bce50
Mattia Torsello <mtorsell@student.42.fr>
2019-03-21 12:54:13 +0100

    protected a bit config.ini

--- srcs/config.c
+++ srcs/config.c
@@ -6,47 +6,6 @@
 #include "libft.h"
 #include "config.h"
 
-t_config    load_ini(int fd)
-{
-    t_config    config;
-    char        *buffer;
-
-    get_next_line_from_char(fd, &buffer, '\n');
-    config.win_w = ft_atoi(buffer);
-    get_next_line_from_char(fd, &buffer, '\n');
-    config.win_h = ft_atoi(buffer);
-    get_next_line_from_char(fd, &buffer, '\n');
-    config.fullscreen = ft_atoi(buffer);
-    get_next_line_from_char(fd, &buffer, '\n');
-    config.fov = ft_atoi(buffer);
-    get_next_line_from_char(fd, &buffer, '\n');
-    config.fps_max = ft_atoi(buffer);
-    get_next_line_from_char(fd, &buffer, '\n');
-    config.mouse_sensi = ft_atoi(buffer) / 1000000.0;
-    close(fd);
-    config.half_w = config.win_w / 2.0;
-    config.half_h = config.win_h / 2.0;
-    config.half_fov = config.fov / 2.0;
-    config.tan_half_fov = tan(config.half_fov);
-    config.ratio = (config.half_w / config.tan_half_fov);
-    return (config);
-}
-
-void        create_config_file(t_config *config)
-{
-    int     fd;
-
-    if ((fd = open(CONFIG_PATH, O_WRONLY | O_CREAT)) < 0)
-        error_doom("couldn't create config file.");
-    ft_putendl_fd(ft_itoa(config->win_w), fd);
-    ft_putendl_fd(ft_itoa(config->win_h), fd);
-    ft_putendl_fd(ft_itoa((int)config->fullscreen), fd);
-    ft_putendl_fd(ft_itoa((int)round(config->fov)), fd);
-    ft_putendl_fd(ft_itoa(config->fps_max), fd);
-    ft_putendl_fd(ft_itoa((int)round(config->mouse_sensi * 1000000)), fd);
-    close(fd);
-}
-
 t_config    load_default_config()
 {
     t_config    config;
@@ -64,6 +23,69 @@ t_config    load_default_config()
     config.tan_half_fov = TAN_HALF_FOV;
     config.ratio = RATIO;
     return (config);
+}
+
+static int  extract_int_if_line_is_valid(char *line)
+{
+    if ((line = ft_strchr(line, ':')) != NULL)
+        return (ft_atoi(line + 1));
+    return (0);
+}
+
+t_config    load_ini(int fd)
+{
+    t_config    config;
+    char        *buffer;
+
+    get_next_line_from_char(fd, &buffer, '\n');
+    config.win_w = extract_int_if_line_is_valid(buffer);
+    get_next_line_from_char(fd, &buffer, '\n');
+    config.win_h = extract_int_if_line_is_valid(buffer);
+    get_next_line_from_char(fd, &buffer, '\n');
+    config.fullscreen = extract_int_if_line_is_valid(buffer);
+    get_next_line_from_char(fd, &buffer, '\n');
+    config.fov = extract_int_if_line_is_valid(buffer);
+    get_next_line_from_char(fd, &buffer, '\n');
+    config.fps_max = extract_int_if_line_is_valid(buffer);
+    get_next_line_from_char(fd, &buffer, '\n');
+    config.mouse_sensi = extract_int_if_line_is_valid(buffer);
+    close(fd);
+    if (config.win_w && config.win_h && config.fov && config.fps_max && config.mouse_sensi)
+    {
+        config.half_w = config.win_w / 2.0;
+        config.half_h = config.win_h / 2.0;
+        config.half_fov = config.fov / 2.0;
+        config.tan_half_fov = tan(config.half_fov);
+        config.ratio = (config.half_w / config.tan_half_fov);
+    }
+    else
+    {
+        config = load_default_config();
+        ft_putendl("There was an issue with config.ini so default config has been loaded.");
+    }
+    return (config);
+}
+
+void        create_config_file(t_config *config)
+{
+    int     fd;
+
+    if ((fd = open(CONFIG_PATH, O_WRONLY | O_CREAT)) < 0)
+        error_doom("couldn't create config file.");
+    ft_putendl_fd(
+            ft_strjoin("resolution x:", ft_itoa(config->win_w)), fd);
+    ft_putendl_fd(
+            ft_strjoin("resolution y:",ft_itoa(config->win_h)), fd);
+    ft_putendl_fd(
+            ft_strjoin("fullscreen:", ft_itoa((int)config->fullscreen)), fd);
+    ft_putendl_fd(
+            ft_strjoin("FOV:",ft_itoa((int)round(config->fov))), fd);
+    ft_putendl_fd(
+            ft_strjoin("FPS :",ft_itoa(config->fps_max)), fd);
+    ft_putendl_fd(
+            ft_strjoin("mouse sensitivity:",
+                    ft_itoa((int)round(config->mouse_sensi * 1000000))), fd);
+    close(fd);
 }
 
 t_config    load_config()
