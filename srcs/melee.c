bbe9e2f5990c34246756ac2a945a7c34637cdeb0
MattDirty <mtorsell@student.42.fr>
2019-03-23 19:16:05 +0100

    renamed animation.end to duration

--- srcs/melee.c
+++ srcs/melee.c
@@ -10,14 +10,14 @@ void    melee_primary_animation(t_animation *animation)
     int step;
     int value;
 
-    step = animation->state / (animation->end / 4);
-    value = animation->state % (animation->end / 4);
+    step = animation->state / (animation->duration / 4);
+    value = animation->state % (animation->duration / 4);
     if (step == 0)
         animation->x_offset = -value;
     if (step == 1)
-        animation->x_offset = -(animation->end / 4) + value;
+        animation->x_offset = -(animation->duration / 4) + value;
     if (step == 2)
         animation->x_offset = value;
     if (step == 3)
-        animation->x_offset = animation->end / 4 - value;
+        animation->x_offset = animation->duration / 4 - value;
 }
