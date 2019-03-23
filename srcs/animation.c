90ee09aa5a28148019945bccc784f76100fd593b
MattDirty <mtorsell@student.42.fr>
2019-03-24 00:54:27 +0100

    Weapon animation uses timer_handler

--- srcs/animation.c
+++ srcs/animation.c
@@ -16,16 +16,3 @@ void    start_animation(t_animation *animation, Uint32 duration)
     animation->time = 0;
     animation->duration = duration;
 }
-
-void    animate(t_weapon *weapon, double ms_since_move)
-{
-    if (weapon->animation.duration == 0
-    || weapon->animation.time >= weapon->animation.duration)
-        return ;
-    weapon->animation.time += ms_since_move;
-    if (weapon->animation.time > weapon->animation.duration)
-        weapon->animation.time = weapon->animation.duration;
-    weapon->animation.animation_controller(&weapon->animation);
-    if (weapon->animation.time == weapon->animation.duration)
-        reset_animation(&weapon->animation);
-}
--- srcs/melee.c
+++ srcs/melee.c
@@ -7,7 +7,32 @@ enum e_bool    unlock(double ms_since_update, const void *ready)
 {
     (void)ms_since_update;
     *(enum e_bool*)ready = t_true;
-    return t_false;
+    return (t_false);
+}
+
+enum e_bool    melee_primary_animation(
+        double ms_since_update,
+        const void *pointer)
+{
+    int step;
+    int value;
+    t_animation *animation;
+
+    animation = (t_animation*)pointer;
+    animation->time += ms_since_update;
+
+    step = animation->time / (animation->duration / 4);
+    value = animation->time % (animation->duration / 4);
+    if (step == 0)
+        animation->x_offset = -value;
+    if (step == 1)
+        animation->x_offset = -(animation->duration / 4) + value;
+    if (step == 2)
+        animation->x_offset = value;
+    if (step == 3)
+        animation->x_offset = animation->duration / 4 - value;
+
+    return (animation->time < animation->duration);
 }
 
 void    melee_secondary(t_weapon *weapon, t_timer_handler *timer_handler)
@@ -26,28 +51,19 @@ void    melee_primary(t_weapon *weapon, t_timer_handler *timer_handler)
 {
     if (!weapon->main_ready)
         return;
+
+    reset_animation(&weapon->animation);
     start_animation(&weapon->animation, 400);
+    add_event(
+            timer_handler,
+            5,
+            &melee_primary_animation,
+            &weapon->animation);
+
     weapon->main_ready = t_false;
     add_event(
             timer_handler,
             weapon->main_cooldown,
             &unlock,
             &weapon->main_ready);
-}
-
-void    melee_primary_animation(t_animation *animation)
-{
-    int step;
-    int value;
-
-    step = animation->time / (animation->duration / 4);
-    value = animation->time % (animation->duration / 4);
-    if (step == 0)
-        animation->x_offset = -value;
-    if (step == 1)
-        animation->x_offset = -(animation->duration / 4) + value;
-    if (step == 2)
-        animation->x_offset = value;
-    if (step == 3)
-        animation->x_offset = animation->duration / 4 - value;
 }
