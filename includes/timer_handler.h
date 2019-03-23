cbf0fb80b1db2c7bb56d6711173b13ba994f82fb
MattDirty <mtorsell@student.42.fr>
2019-03-23 23:13:41 +0100

    Added possibility for events to stay alive and keep firing each interval

--- includes/timer_handler.h
+++ includes/timer_handler.h
@@ -1,12 +1,16 @@
 #ifndef DOOM_NUKEM_TIMER_HANDLER_H
 # define DOOM_NUKEM_TIMER_HANDLER_H
 
+# include "e_bool.h"
+
 typedef const void *t_params;
 typedef void (*t_event_func)(t_params);
 
 typedef struct    s_event
 {
-    double time_left;
+    double interval;
+	double time_left;
+	enum e_bool destroy_after_call;
     t_event_func function;
     t_params params;
     struct s_event *next;
@@ -25,7 +29,8 @@ void timer_handler_init(t_timer_handler *timer_handler);
 void update_events(t_timer_handler *timer_handler);
 void add_event(
         t_timer_handler *timer_handler,
-        double time_until,
+        double interval,
+        enum e_bool destroy_after_call,
         t_event_func function,
         t_params params);
 
