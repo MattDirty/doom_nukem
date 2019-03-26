53d95536c410e62b7a0f2c92641bcfc9750652b9
roflolilolmao <qjeanmon@gmail.com>
2019-03-26 23:03:13 +0100

    Whole file serialisation

--- srcs/serialisation.c
+++ srcs/serialisation.c
@@ -1,9 +1,44 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "libft.h"
 
 #include "serialisation.h"
+
+int	read_file(char *filename, t_textures **textures, t_map **map)
+{
+    int fd;
+
+    fd = open(filename, O_RDONLY);
+    if (fd <= 0)
+        return (-1);
+
+    if (read_textures_from_file(fd, textures) < 0)
+        return (-2);
+
+    if (read_map_from_file(fd, *textures, map) < 0)
+        return (-3);
+
+    return (0);
+}
+
+int	write_file(char *filename, t_textures *textures, t_map *map)
+{
+    int fd;
+
+    fd = open(filename, O_WRONLY | O_CREAT, 777);
+    if (fd <= 0)
+        return (-1);
+
+    if (write_textures_to_file(fd, textures) < 0)
+        return (-2);
+
+    if (write_map_to_file(fd, map) < 0)
+        return (-3);
+
+    return (0);
+}
 
 int	read_str_from_file(int fd, char **name)
 {
