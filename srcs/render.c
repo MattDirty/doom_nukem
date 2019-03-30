e9a9487dfa1f9b3c18fe38cf80675181bbd00921
lfatton <louise.fatton@hotmail.fr>
2019-03-30 19:46:57 +0100

    working floorcasting

--- srcs/render.c
+++ srcs/render.c
@@ -16,25 +16,24 @@
 #include "skybox.h"
 #include "surface_manipulation.h"
 
-static void	draw_floor(t_env *e, t_collision collision, t_coords bottom_of_wall, t_coords player_pos , SDL_Surface *derp)
+static void	draw_floor(t_env *e, t_collision collision, Uint32 renderer_x, SDL_Surface *floor)
 {
     double      pixel_dist;
-    double      weight_factor;
-    t_coords    floor_pos;
+    double      weight;
     t_coords    draw_text;
     Uint32      color_text;
+    Uint32      y;
 
-    while (bottom_of_wall.y + 1 < WIN_H)
+    y = e->p.vision_height + collision.wall_length / 2 + 1;
+    while (y < WIN_H)
     {
-        pixel_dist = e->op.win_h / (2.0 * bottom_of_wall.y - e->op.win_h);
-        weight_factor = (pixel_dist / collision.distance);
-        floor_pos.x = weight_factor * collision.inters.x + (1.0 - weight_factor) * player_pos.x;
-        floor_pos.y = weight_factor * collision.inters.y + (1.0 - weight_factor) * player_pos.y;
-        draw_text.x = (int)(floor_pos.x * PIXEL_UNIT) % derp->w;
-        draw_text.y = (int)(floor_pos.y * PIXEL_UNIT) % derp->h;
-        color_text = get_pixel(derp, draw_text.x, draw_text.y, t_true);
-        put_pixel(e->doom.surface, bottom_of_wall.x, bottom_of_wall.y, color_text);
-        bottom_of_wall.y++;
+        pixel_dist = e->op.win_h / (y - e->p.vision_height);
+        weight = (pixel_dist / collision.distance);
+        draw_text.x = (Uint32)((weight * collision.inters.x + (3 - weight) * e->p.pos.x) * floor->w) % floor->w;
+        draw_text.y = (Uint32)((weight * collision.inters.y + (3 - weight) * e->p.pos.y) * floor->h) % floor->h;
+        color_text = get_pixel(floor, draw_text.x, draw_text.y, t_true);
+        put_pixel(e->doom.surface, renderer_x, y, color_text);
+        y++;
     }
 }
 
@@ -48,7 +47,7 @@ static void	draw_wall(t_env *e, t_collision collision, Uint32 renderer_x, double
 	y = vision_height - collision.wall_length / 2;
     y = (y < 0 ? 0 : y);
     end = vision_height + collision.wall_length / 2;
-    end = (end > (int)e->op.win_h ? e->op.win_h : end);
+	end = (end > e->op.win_h ? e->op.win_h : end);
 	draw_text.x = (int)(get_distance_between_points(collision.inters.x,
 	        collision.inters.y, collision.wall->segment.x1,
 	        collision.wall->segment.y1) * PIXEL_UNIT) % collision.wall->texture->w;
@@ -80,17 +79,11 @@ void		draw(
 		t_collision collision,
 		Uint32 renderer_x)
 {
-    t_coords    bottom_of_wall;
+	collision.distance *= cos(e->p.heading - ray_angle);
+	collision.wall_length = e->op.ratio / collision.distance * collision.wall->height;
 
-	collision.distance *= cos(e->p->heading - ray_angle);
-	collision.wall_length = RATIO / collision.distance * collision.wall->height;
-
-    bottom_of_wall.x = renderer_x;
-	bottom_of_wall.y = e->p.vision_height + collision.wall_length / 2;
-    bottom_of_wall.y = (bottom_of_wall.y > WIN_H ? WIN_H : bottom_of_wall.y);
-
-	paint_floor(e, &e->op, renderer_x, e->p.vision_height);
-	skybox(e, renderer_x);
+    paint_floor(e, &e->op, renderer_x, e->p.vision_height);
+    skybox(e, renderer_x);
 	draw_wall(e, collision, renderer_x, e->p.vision_height);
-	draw_floor(e, collision, bottom_of_wall, e->p.pos, e->derp);
+    draw_floor(e, collision, renderer_x, e->derp);
 }
