72d47eb60ba18dd5c4f7cd4f5667fd73388cda5e
Louise FATTON <lfatton@student.42.fr>
2019-03-31 03:04:07 +0200

    refactor render.c

--- includes/collision.h
+++ includes/collision.h
@@ -1,18 +1,19 @@
-#ifndef DOOMNUKEM_COLLISION_H
-#define DOOMNUKEM_COLLISION_H
+#ifndef COLLISION_H
+# define COLLISION_H
 
-#include "utils.h"
-#include "walls.h"
-#include "sectors.h"
+# include "utils.h"
+# include "walls.h"
+# include "sectors.h"
+# include "doom.h"
 
 typedef struct          s_collision
 {
     double              distance;
-    double              wall_length;
     t_coords            inters;
     t_wall				*wall;
 }                       t_collision;
 
 t_collision	check_collision(t_sector *sector, t_segment *seg);
+void		raycasting(t_env *e);
 
 #endif
--- includes/engine.h
+++
@@ -1,9 +0,0 @@
-#ifndef DOOMNUKEM_ENGINE_H
-#define DOOMNUKEM_ENGINE_H
-
-#include "collision.h"
-
-void		raycasting(t_env *e);
-void		draw(t_env *e, double ray_angle, t_collision collision, Uint32 renderer_x);
-
-#endif
