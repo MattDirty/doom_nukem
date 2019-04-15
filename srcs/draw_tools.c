4decdb40c78a8a62ca3d91671ef47f34f1482762
MattDirty <mtorsell@student.42.fr>
2019-04-15 16:23:37 +0200

    normed Bresenham

--- srcs/draw_tools.c
+++ srcs/draw_tools.c
@@ -2,7 +2,7 @@
 #include "surface_manipulation.h"
 #include <math.h>
 
-enum e_bool is_bresenham_out_of_bounds(t_segment *seg, t_i_coords *incr)
+enum e_bool is_bresenham_finished(t_segment *seg, t_i_coords *incr)
 {
     if ((incr->x > 0 && (int)seg->x1 > (int)seg->x2)
         || (incr->x < 0 && (int)seg->x1 < (int)seg->x2)
@@ -12,35 +12,31 @@ enum e_bool is_bresenham_out_of_bounds(t_segment *seg, t_i_coords *incr)
     return (t_false);
 }
 
-void draw_segment(SDL_Surface *surface, t_segment segment, Uint32 color)
+void draw_segment(SDL_Surface *surface, t_segment s, Uint32 color)
 {
     t_i_coords incr;
     int err;
     int cpyerr;
     t_i_coords delta;
 
-    delta.x = fabs(segment.x2 - segment.x1);
-    delta.y = fabs(segment.y2 - segment.y1);
-    incr.x = segment.x1 < segment.x2 ? 1 : -1;
-    incr.y = segment.y1 < segment.y2 ? 1 : -1;
+    delta = (t_i_coords){fabs(s.x2 - s.x1), fabs(s.y2 - s.y1)};
+    incr = (t_i_coords){s.x1 < s.x2 ? 1 : -1, s.y1 < s.y2 ? 1 : -1};
     err = (delta.x > delta.y ? delta.x : -delta.y) / 2;
-    while (((int)segment.x1 != (int)segment.x2
-    || (int)segment.y1 != (int)segment.y2)
-    && !is_bresenham_out_of_bounds(&segment, &incr))
+    while (((int)s.x1 != (int)s.x2 || (int)s.y1 != (int)s.y2)
+    && !is_bresenham_finished(&s, &incr))
     {
-        if (segment.x1 >= 0 && segment.x1 < surface->w
-        && segment.y1 >= 0 && segment.y1 < surface->h)
-            put_pixel(surface, (int) segment.x1, (int) segment.y1, color);
+        if (s.x1 >= 0 && s.x1 < surface->w && s.y1 >= 0 && s.y1 < surface->h)
+            put_pixel(surface, (int) s.x1, (int) s.y1, color);
         cpyerr = err;
         if (cpyerr > -(delta.x))
         {
             err -= delta.y;
-            segment.x1 += incr.x;
+            s.x1 += incr.x;
         }
         if (cpyerr < delta.y)
         {
             err += delta.x;
-            segment.y1 += incr.y;
+            s.y1 += incr.y;
         }
     }
 }
