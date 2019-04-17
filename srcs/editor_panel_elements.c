129f020527a06f2b3dc97f4a3fda1134a4cf082d
MattDirty <mtorsell@student.42.fr>
2019-04-17 08:29:37 +0200

    it works now ! Can't move a wall node inside another sector.

--- srcs/editor_panel_elements.c
+++ srcs/editor_panel_elements.c
@@ -19,7 +19,7 @@ void        split_wall(t_params params)
     wall->segment.x2 = new_wall->segment.x1;
     wall->segment.y2 = new_wall->segment.y1;
 	add_wall_to_sector(sector, new_wall);
-	free(btn_params->ed->linked_walls);
+	free_linked_walls_nodes(btn_params->ed->linked_walls);
 	create_linked_walls_from_sectors(
 			btn_params->ed->map->sectors,
 			&btn_params->ed->linked_walls,
