05eeb379701a69c298d707d931ffbbbaa796aa98
Louise FATTON <lfatton@student.42.fr>
2019-04-18 09:37:17 +0200

    ouuuuuups

---
+++ srcs/vacuum.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vacuum.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfatton <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/18 08:20:57 by lfatton           #+#    #+#             */
+/*   Updated: 2019/04/18 08:20:58 by lfatton          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "weapon.h"
+#include "doom.h"
+#include "default.h"
+
+void    vacuum_primary(t_env *e, t_timer_handler *timer_handler)
+{
+    t_weapon            *weapon;
+
+    weapon = e->p.weapon;
+    if (!weapon->main_ready || !weapon->ammo)
+        return;
+    Mix_PlayChannel(-1, weapon->main_sound, 0);
+    weapon->main_ready = e_false;
+    weapon_ray_fire(e, timer_handler);
+}
+
+t_weapon    *load_vacuum(t_sounds *sounds, t_map *map)
+{
+    t_weapon    *vacuum;
+
+    if (!(vacuum = (t_weapon *)malloc(sizeof(t_weapon))))
+        error_doom("Couldn't malloc vacuum");
+    vacuum->sprites_count = 1;
+    if (!(vacuum->sprites = (SDL_Surface **)malloc(sizeof(SDL_Surface *)
+                                                   * vacuum->sprites_count)))
+        error_doom("Couldn't malloc vacuum.sprites");
+    vacuum->sprites[0] = map->vacuum_sprite;
+    vacuum->sprites_cooldown = NULL;
+    vacuum->sprites_fire = NULL;
+    vacuum->sprite_current = vacuum->sprites[0];
+    vacuum->ammo = -1;
+    vacuum->usable = e_true;
+    vacuum->main = vacuum_primary;
+    vacuum->main_cooldown = 60000;
+    vacuum->main_ready = e_true;
+    if (!(vacuum->main_sound = sounds->slurp))
+        error_doom("Can't load vacuum sound ...");
+    vacuum->range = HORIZON;
+    vacuum->scatter = 30;
+    vacuum->scatter_angle = 0.785398;
+    vacuum->damage = 500000;
+    return (vacuum);
+}
