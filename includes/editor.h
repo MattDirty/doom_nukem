c78845f3a4c53d3ff5f225d52f1c166f91e79b72
Baptiste <baptiste@dhont.me>
2019-04-19 03:36:04 +0200

    ok

--- includes/editor.h
+++ includes/editor.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   editor.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: badhont <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/19 03:10:13 by badhont           #+#    #+#             */
+/*   Updated: 2019/04/19 06:55:17 by badhont          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef DOOMNUKEM_EDITOR_H
 # define DOOMNUKEM_EDITOR_H
 
@@ -24,16 +36,16 @@
 # define EDITOR_ZOOM 20
 # define CORNER_SIZE 10
 
-typedef struct  s_wall_nodes t_wall_nodes;
+typedef	struct	s_wall_nodes t_wall_nodes;
 
-typedef struct			s_sdl_editor
+typedef struct				s_sdl_editor
 {
-    SDL_Window			*window;
-    SDL_Renderer		*renderer;
-    SDL_Surface         *surface;
-}						t_sdl_editor;
+	SDL_Window			*window;
+	SDL_Renderer		*renderer;
+ 	SDL_Surface			*surface;
+}							t_sdl_editor;
 
-typedef	struct			s_selected_elements
+typedef	struct				s_selected_elements
 {
 	t_wall_nodes		*nodes;
 	t_coords			*p_spawn;
@@ -44,7 +56,7 @@ typedef	struct			s_selected_elements
 	t_wall              *wall;
 }						t_selected_elements;
 
-enum    e_editor_state
+enum	e_editor_state
 {
     e_null = 0,
     e_add_object,
@@ -55,9 +67,9 @@ enum    e_editor_state
     e_add_pickable
 };
 
-typedef struct          s_editor
+typedef struct				s_editor
 {
-	t_sdl_editor    	sdl;
+	t_sdl_editor		sdl;
 	char				*map_path;
     t_map				*map;
     t_textures      	*textures;
@@ -77,9 +89,9 @@ typedef struct          s_editor
 	t_selected_elements	selected;
 	t_selected_elements	dragged;
 	t_linked_walls		*linked_walls;
-	int 				linked_walls_count;
-	enum e_bool         map_is_updated;
-}                       t_editor;
+	int					linked_walls_count;
+	enum e_bool			map_is_updated;
+}						t_editor;
 
 t_map		*create_map(t_textures *textures);
 t_textures	*load_textures(void);
@@ -88,7 +100,7 @@ void		editor_loop(t_editor *ed);
 void    		mousedown_action(t_editor *ed, int mouse_x, int mouse_y);
 void    		mouseup_action(t_editor *ed, int mouse_x, int mouse_y);
 enum e_bool		click_on_nodes(t_editor *ed, t_linked_walls *linked_walls, int mouse_x, int mouse_y);
-void            deal_with_clicked_wall(t_editor *ed, t_wall *wall);
+void			deal_with_clicked_wall(t_editor *ed, t_wall *wall);
 void		quit_editor(t_editor *ed);
 void		create_sub_lists(t_textures *textures, t_panel *panel);
 t_sounds	*load_sounds(void);
