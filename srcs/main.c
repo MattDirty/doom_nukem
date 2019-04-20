099de7376de2606dd4fc6fc51654c21a4ba26c69
Louise FATTON <lfatton@student.42.fr>
2019-04-20 02:45:19 +0200

    main done

--- srcs/main.c
+++ srcs/main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfatton <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/20 02:25:40 by lfatton           #+#    #+#             */
+/*   Updated: 2019/04/20 02:25:42 by lfatton          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <SDL.h>
 #include <SDL_mixer.h>
 #include <SDL_ttf.h>
@@ -10,30 +22,27 @@
 
 t_sdl	init_sdl(Uint32 w, Uint32 h, Uint32 fullscreen, char *name)
 {
-	t_sdl   sdl;
+	t_sdl	sdl;
 
 	fullscreen = SDL_WINDOW_FULLSCREEN_DESKTOP;
-	if (!(sdl.window = SDL_CreateWindow(name, 0, 0,
-				w, h, fullscreen)))
+	if (!(sdl.window = SDL_CreateWindow(name, 0, 0, w, h, fullscreen)))
 		error_doom("Could not create window.");
 	if (!(sdl.renderer = SDL_CreateRenderer(sdl.window, -1, 0)))
 		error_doom("Could not create renderer");
 	if (!(sdl.texture = SDL_CreateTexture(sdl.renderer,
-			SDL_PIXELFORMAT_ARGB32, SDL_TEXTUREACCESS_TARGET,
-			w, h)))
+			SDL_PIXELFORMAT_ARGB32, SDL_TEXTUREACCESS_TARGET, w, h)))
 		error_doom("Could not create texture.");
-	if (!(sdl.surface = SDL_CreateRGBSurface(0, w, h,
-				32, MASK_RED, MASK_GREEN, MASK_BLUE, MASK_ALPHA)))
+	if (!(sdl.surface = SDL_CreateRGBSurface(0, w, h, 32, MASK_RED, MASK_GREEN,
+			MASK_BLUE, MASK_ALPHA)))
 		error_doom("Could not create surface.");
-	if (Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 2, 2048 ) < 0 )
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
 		error_doom((char *)Mix_GetError());
-
 	return (sdl);
 }
 
-int		main (int ac, char **av)
+int		main(int ac, char **av)
 {
-	t_env		e;
+	t_env	e;
 
 	if (ac != 2)
 		error_doom("Usage is : ./doom-nukem path_to_map");
@@ -48,7 +57,7 @@ int		main (int ac, char **av)
 	if (!(e.music = e.sounds->lamerde))
 		error_doom("Couldn't open lamerde.wav");
 	e.p = init_player(&e.op, e.map->spawn,
-		e.map->sectors->items[e.map->player_spawn_index]);
+			e.map->sectors->items[e.map->player_spawn_index]);
 	e.p.weapons = allocate_weapons(e.sounds, e.map);
 	e.p.weapon = e.p.weapons->item;
 	loop_doom(&e);
